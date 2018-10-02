cd1c3a31224bac6a58a46d2c172eac2e0da609ba
tannerkrewson <tannerkrewson@gmail.com>
2018-10-01 21:21:58 -0500

    add an example

--- main.py
+++ main.py
@@ -30,9 +30,31 @@ def main():
     print("Random was right " + str(randomWasRight/numberOfFilesScanned * 100) + "% of the time")
 
 def findBestORF(orfs):
-    # PUT OUR CODE HERE
-    return randint(0, 5)
+    print()
 
+    #
+    # PUT OUR CODE HERE
+    #
+
+    # example:
+    # counts number of start codons
+    # assumes that the more start codons there are, 
+    # the better the orf (not saying that's right,
+    # just an example)
+
+    bestORF = 0
+    maxStartCodonAmount = 0
+    for idx, orf in enumerate(orfs):
+        orfStartCount = countStart(orf)
+        print("ORF" + str(idx) + " has " + str(orfStartCount) + " start codons")
+
+        if orfStartCount >= maxStartCodonAmount:
+            bestORF = idx
+            maxStartCodonAmount = orfStartCount
+
+    print("The best ORF is obviously ORF" + str(bestORF) + "!")
+
+    return bestORF
 
 def getAllOrfs(gene):
     orfList = []
@@ -40,9 +62,10 @@ def getAllOrfs(gene):
     orfList.append(gene[1:]) # not including first character of string
     orfList.append(gene[2:]) # not including first or second
 
-    orfList.append(gene[::-1]) # reverse the string
-    orfList.append(gene[1::-1])
-    orfList.append(gene[2::-1])
+    rev = gene[::-1]
+    orfList.append(rev) # reverse the string
+    orfList.append(rev[1:])
+    orfList.append(rev[2:])
 
     return orfList
 
