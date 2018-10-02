1ca449ace654ccab780f0a950ed7ad34fbf5f759
tannerkrewson <tannerkrewson@gmail.com>
2018-10-01 23:13:34 -0500

    Cleaned up and clarified names

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ def main():
     randomWasRight = 0
     weWereRight = 0
     numberOfFilesScanned = 0
-    ACTUAL_ORF = 1
+    ACTUAL_READING_FRAME = 1
 
     os.chdir(os.getcwd() + "/genes/")
     for file in glob.glob("*.fsa"):
         gene = readfasta(file)[0][1]
-        orfs = getAllOrfs(gene)
+        rfs = getAllReadingFrames(gene)
 
-        if randint(0, 5) == ACTUAL_ORF:
+        if randint(0, 5) == ACTUAL_READING_FRAME:
             randomWasRight = randomWasRight + 1
 
-        if findBestORF(orfs) == ACTUAL_ORF:
+        if findBestReadingFrame(rfs) == ACTUAL_READING_FRAME:
             weWereRight = weWereRight + 1
 
         numberOfFilesScanned = numberOfFilesScanned + 1
@@ -29,66 +29,48 @@ def main():
     print("Our program was right " + str(weWereRight/numberOfFilesScanned * 100) + "% of the time")
     print("Random was right " + str(randomWasRight/numberOfFilesScanned * 100) + "% of the time")
 
-def findBestORF(orfs):
+def findBestReadingFrame(rfs):
     #
     # PUT OUR CODE HERE
     #
 
-    orf_scores = [0, 0, 0, 0, 0, 0]
+    rf_scores = [0, 0, 0, 0, 0, 0]
     possible_orf_list = []
 
-    for idx, reading_frame in enumerate(orfs):
-        thisOrfScore = 0
+    # rf_number: the six possible reading frames, 0 through 5
+    # rf_bases: a string with all the bases of the reading frame
+    for rf_number, rf_bases in enumerate(rfs):
+        thisRfScore = 0
 
-        porf = possible_orfs(reading_frame)
+        # gets list of pairs of the start and stop indexes of the
+        # possible orfs in the rf_number-th reading frame
+        porf = possible_orfs(rf_bases)
         possible_orf_list.append(porf)
 
+        # if it has a orf, increase the score
         if (len(porf) > 0):
-            # if it has a orf, increase the score
-            orf_scores[idx] = 1
+            rf_scores[rf_number] = 1
 
     # get the index of the best reading frame score
-    bestORF = orf_scores.index(max(orf_scores))
+    bestRF = rf_scores.index(max(rf_scores))
 
-    print(orf_scores)
-    print("The best ORF is ORF" + str(bestORF) + "!")
+    print(rf_scores)
+    print("The best ORF is ORF" + str(bestRF) + "!")
 
-    return bestORF
-def getAllOrfs(gene):
-    orfList = []
-    orfList.append(gene)
-    orfList.append(gene[1:]) # not including first character of string
-    orfList.append(gene[2:]) # not including first or second
+    return bestRF
+
+def getAllReadingFrames(gene):
+    rfList = []
+    rfList.append(gene)
+    rfList.append(gene[1:]) # not including first character of string
+    rfList.append(gene[2:]) # not including first or second
 
     rev = gene[::-1]
-    orfList.append(rev) # reverse the string
-    orfList.append(rev[1:])
-    orfList.append(rev[2:])
+    rfList.append(rev) # reverse the string
+    rfList.append(rev[1:])
+    rfList.append(rev[2:])
 
-    return orfList
-
-def translateToAminoAcid(dna):
-    rna = dna.replace('T', 'U')
-
-    aa_sequence = ''
-
-    for i in range(0, len(rna), 3):
-        codon = rna[i:i + 3]
-        if len(codon) == 3:
-            aa = code[codon]
-            aa_sequence += aa
-
-    return aa_sequence
-
-def countStart(dna):
-    rna = dna.replace('T', 'U')
-    count = 0
-    for i in range(0, len(rna), 3):
-        codon = rna[i:i + 3]
-        if codon == 'AUG':
-            count += 1
-
-    return count
+    return rfList
 
 #outputs possible orfs of over length 50 codons and returns them as a list
 ##of pairs of the index of the first and last basepair
